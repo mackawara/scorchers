ebf72055f878d6d09a6bcc7d3ecf34a671b6c3e8
mackawara <mkawara@outlook.com>
2022-10-10 11:47:56 +0100

    feat : refactored typwriter finction

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,13 +4,22 @@ window.addEventListener("DOMContentLoaded", () => {
   const txt =
     "Scorchers Cricket: Nurturing the next generation of cricket stars."; /* The text */
   const speed = 100; /* The speed/duration of the effect in milliseconds */
-
-  function typeWriter() {
-    if (i < txt.length) {
-      document.getElementById("nurturing").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
+const nurt=document.getElementById("nurturing")
+ 
+  //typeWriter();
+  async function typeWriter(element,text){
+    const timer = (ms) => new Promise((res) => setTimeout(res, ms))
+    
+    console.log(element,text)
+    for (let index = 0; index <text.length; index++) {
+      const letter =text.charAt(index);
+      element.innerHTML+=letter
+      console.log(letter)
+     await timer(100)
+     // setTimeout(element.innerHTML+=letter,5000)
+      
     }
+
   }
-  typeWriter();
+  typeWriter(nurt,txt)
 });
